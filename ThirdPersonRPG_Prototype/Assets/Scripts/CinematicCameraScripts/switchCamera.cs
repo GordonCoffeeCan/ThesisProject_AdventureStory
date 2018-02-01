fefd09fbe1493fc781acb6c1ca5678ea0aefb096
Mostafa Haque <93haque@gmail.com>
2018-02-01 15:46:22 -0500

    cinemachine added

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/CinematicCameraScripts/switchCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCamera : MonoBehaviour {
+    public GameObject playerCamera;
+    public GameObject cinemachineCamera;
+
+	// Use this for initialization
+	void Start () {
+        cinemachineCamera.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            if (playerCamera.activeInHierarchy) {
+                playerCamera.SetActive(false);
+                cinemachineCamera.SetActive(true);
+            }
+            else {
+                playerCamera.SetActive(true);
+                cinemachineCamera.SetActive(false);
+            }
+        }
+
+    }
+}
---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/CinematicCameraScripts/triggerCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerCamera : MonoBehaviour {
+
+    public GameObject vCam;
+   
+
+    private GameObject gameManager;
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll) {
+        gameManager.GetComponent<cameraActivateTrigger>().currentVirtualCamera.SetActive(false);
+        gameManager.GetComponent<cameraActivateTrigger>().currentVirtualCamera = vCam;
+
+        
+
+    }
+}
