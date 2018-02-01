802545e573467d8a00ee518e516cd5eb402b84de
Mostafa Haque <93haque@gmail.com>
2018-02-01 17:01:19 -0500

    camerawtf

--- ThirdPersonRPG_Prototype/Assets/Scripts/CinematicCameraScripts/switchCamera.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/CinematicCameraScripts/switchCamera.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class switchCamera : MonoBehaviour {
-    public GameObject playerCamera;
-    public GameObject cinemachineCamera;
+    //public GameObject playerCamera;
+    public GameObject mainCamera;
+    public GameObject fixedCamera;
+
+    private bool mainCameraActive;
+    private bool fixedCameraActive;
 
 	// Use this for initialization
 	void Start () {
-        cinemachineCamera.SetActive(false);
+        //playerCamera.GetComponent<CinemachineBrain>().enabled = false;
+        mainCamera.SetActive(true);
+        fixedCamera.SetActive(false);
+
+        mainCameraActive = true;
+        fixedCameraActive = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.C)) {
-            if (playerCamera.activeInHierarchy) {
-                playerCamera.SetActive(false);
-                cinemachineCamera.SetActive(true);
-            }
-            else {
-                playerCamera.SetActive(true);
-                cinemachineCamera.SetActive(false);
-            }
+            // playerCamera.GetComponent<CinemachineBrain>().enabled = !dynamicCamera;
+
+            mainCameraActive = !mainCameraActive;
+            fixedCameraActive = !fixedCameraActive;
+
+            mainCamera.SetActive(mainCameraActive);
+            fixedCamera.SetActive(fixedCameraActive);
+            
+            
         }
+
+       
 
     }
 }
