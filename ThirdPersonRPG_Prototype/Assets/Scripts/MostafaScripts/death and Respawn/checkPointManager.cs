83f7895f0144a6398d35dbd0911fc8f70ff37edb
Mostafa Haque <93haque@gmail.com>
2018-02-20 16:14:03 -0500

    new checkpoint/DeathSystem

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/death and Respawn/checkPointManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPointManager : MonoBehaviour {
+    public Vector3 checkPointPos;
+
+	// Use this for initialization
+	void Start () {
+        checkPointPos = GameObject.Find("Player").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/death and Respawn/death.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/death and Respawn/death.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class death : MonoBehaviour {
-
+    public checkPointManager chk;
 	// Use this for initialization
 	void Start () {
+
+        chk = GameObject.Find("GameManager").GetComponent<checkPointManager>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 		
 	}
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.transform.tag == "Player") {
+
+            coll.transform.position = chk.checkPointPos;
+        }
+        
+    }
 }
