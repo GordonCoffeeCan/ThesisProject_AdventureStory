9d3f9a5fd4655afa4f8e1a985e8180633f0bab24
Mostafa Haque <93haque@gmail.com>
2018-02-22 15:35:34 -0500

    boat animations and playerSnapping

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/disableCharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableCharacterController : MonoBehaviour {
+    private GameObject player;
+
+    [SerializeField]
+    private Animator playerTposeAnimator;
+
+    [SerializeField]
+    private float playerYPos = 0.52f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+
+        playerTposeAnimator = player.GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void disableController() {
+        player.GetComponent<PlayerController>().enabled = false;
+        playerTposeAnimator.Play("Human_Idle");
+        
+    }
+
+    void enableController() {
+        player.GetComponent<PlayerController>().enabled = true;
+    }
+}
