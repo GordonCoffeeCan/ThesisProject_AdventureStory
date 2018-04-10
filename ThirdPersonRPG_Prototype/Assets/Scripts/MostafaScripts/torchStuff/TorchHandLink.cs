bfaf45ccb9bc001ad1adee323970723014167e09
Mostafa Haque <93haque@gmail.com>
2018-04-10 14:40:59 -0400

    fire(?)

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/torchStuff/TorchHandLink.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchHandLink : MonoBehaviour {
+    public GameObject hand;
+
+	// Use this for initialization
+	void Start () {
+        hand = GameObject.Find("mixamorig:LeftHand");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = hand.transform.position;
+	}
+}
---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/torchStuff/torchManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torchManager : MonoBehaviour {
+
+    private GameObject player;
+    private PlayerController playerController;
+
+    public bool isHoldingTorch;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        playerController = player.GetComponent<PlayerController>();
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isHoldingTorch == true) {
+            playerController.hasTorch = true;
+       
+
+        }
+    }
+}
