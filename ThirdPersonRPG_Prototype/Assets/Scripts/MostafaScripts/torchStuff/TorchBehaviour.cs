4cbb774e955414d7ec2edb08efb5990c94cf4f96
Mostafa Haque <93haque@gmail.com>
2018-04-10 15:16:43 -0400

    First level collider polish

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/torchStuff/TorchBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchBehaviour : MonoBehaviour {
+
+    private PlayerController playerController;
+
+	// Use this for initialization
+	void Start () {
+
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        this.transform.GetChild(0).gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (playerController.hasTorch == true) {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            this.transform.GetChild(0).gameObject.SetActive(true);
+        }
+		
+	}
+}
