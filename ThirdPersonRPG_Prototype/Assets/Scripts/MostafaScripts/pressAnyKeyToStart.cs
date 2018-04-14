0032c0ac735021b5ec0f883078c5da5d159deabb
Mostafa Haque <93haque@gmail.com>
2018-04-14 12:58:07 -0400

    new builds

---
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/pressAnyKeyToStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pressAnyKeyToStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey) {
+            sceneChange();
+        }
+    }
+
+    public void sceneChange() {
+        int i = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(i + 1);
+    }
+}
