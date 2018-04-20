fc8c76c28d2682cde3eeade955c5f83c8edc4e8e
Mostafa Haque <93haque@gmail.com>
2018-04-19 21:26:33 -0400

    synced footsteps and landing

--- ThirdPersonRPG_Prototype/Assets/Scripts/MultiplayerScripts/Mostafa's Scripts/TriggerPlayAudio.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MultiplayerScripts/Mostafa's Scripts/TriggerPlayAudio.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerPlayAudio : MonoBehaviour {
 
     private bool hasTriggered = false;
     public AudioSource audio;
 
+   
+    public string subtitleText;
+
+    private Text targetSubtitleText;
+
+
 	// Use this for initialization
 	void Start () {
         GameObject playerCam = GameObject.Find("PlayerCamera").gameObject;
 
         audio = playerCam.GetComponent<AudioSource>();
+
+        targetSubtitleText = GameObject.Find("SubtitleText").GetComponent<Text>();
+
+        Debug.Log(subtitleText);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (hasTriggered == true) {
+            if (audio.isPlaying == false) {
+                targetSubtitleText.text = "";
+            }
+        }
+       
 	}
 
     private void OnTriggerEnter(Collider _col) {
@@ -33,8 +49,13 @@ public class TriggerPlayAudio : MonoBehaviour {
 
                 em.rateOverTime = 0;
 
+                Debug.Log(subtitleText);
+                targetSubtitleText.text = "";
+                targetSubtitleText.text += subtitleText;
+
                 
-                
+
+
             }
         }
     }
