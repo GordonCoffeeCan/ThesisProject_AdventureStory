98671bf99d30b964fc633cc626f6c22dddfe99d3
Mostafa Haque <93haque@gmail.com>
2018-05-05 10:44:27 -0400

    level changes

--- ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/environmentalScripts/sun.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/environmentalScripts/sun.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class sun : MonoBehaviour {
+    public float rotationFloat;
+
 
     // Use this for initialization
     // Use this for initialization
@@ -12,7 +14,7 @@ public class sun : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        transform.RotateAround(Vector3.zero, Vector3.right, 1f * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, rotationFloat * Time.deltaTime);
         transform.LookAt(Vector3.zero);
     }
 }
