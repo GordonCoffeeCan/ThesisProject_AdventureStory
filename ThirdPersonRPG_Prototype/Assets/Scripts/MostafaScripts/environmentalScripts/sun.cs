d21f3fe31c3b700b6c9919c51bcb3f64f38f828d
Mostafa Haque <93haque@gmail.com>
2018-05-13 00:58:56 -0400

    New Start Screen

--- ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/environmentalScripts/sun.cs
+++ ThirdPersonRPG_Prototype/Assets/Scripts/MostafaScripts/environmentalScripts/sun.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class sun : MonoBehaviour {
     public float rotationFloat;
+  
 
 
     // Use this for initialization
     // Use this for initialization
     void Start() {
-
+        
     }
 
     // Update is called once per frame
     void Update() {
         transform.RotateAround(Vector3.zero, Vector3.right, rotationFloat * Time.deltaTime);
         transform.LookAt(Vector3.zero);
+      
     }
 }
